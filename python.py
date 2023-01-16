2a03627fbf0cf9df355e6f3877adaeb1c22496ce
shahedcsse <shahedhasan920@gmail>
2023-01-16 07:27:27 -0800

    first day

---
+++ python.py
@@ -0,0 +1,43 @@
+name= input("Enter Your Name :")
+name=name +"!"
+print(name)
+
+#data type
+name="shahed hasan"
+name2='shahed chowdhury'
+name2
+name
+#string function
+name.capitalize()
+name.title()
+name.upper()
+name.lower()
+name.count('s')
+"shahedhasan920@gmail.com".endswith("@gmail.com")
+"shahedhasan920@gmail.com".startswith("shahed")
+"shahedhasan920@gmail.com".split("@")
+#slicing 
+#index number of string 
+name3="shahedhasan920@gmail.com"
+name3[5:19]
+name3[:20]
+name3[1:]
+name3[1:12:2]
+#Revers count string 
+# #nagative index
+name3[-1:-12:-1] 
+#full revers
+name3[::-1]
+
+#complex data type
+c=1+3j
+c.real
+c.imag
+
+#type casting
+data=223  #int data
+float(data)
+str(data)
+complex(data)
+bool(data)
+
