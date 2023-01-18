23a0fcc80062477fd658a7370318d7ad6fccaa8d
shahedcsse <shahedhasan920@gmail>
2023-01-17 21:26:26 -0800

    3rd day

---
+++ operation.py
@@ -0,0 +1,64 @@
+2>3
+#Logical operator 
+False and False
+True and True
+True and False
+#
+True or False
+True or False
+True or False
+#
+not False
+not True 
+
+#Asingment operator
+num=3
+num=num+2
+print (num)
+#another option 
+num=44
+num+=4
+print (num)
+num-=5
+print(num)
+num**=2
+print(num)
+num%=4
+print(num)
+num/=4
+print(num)
+
+#spacial opertor
+
+cheek="py"in"python"
+print(cheek)
+#int value
+hello=4 in[3,5,2,32,14]
+print (hello) 
+
+#is operator 
+v= "python"
+u="python"
+sum=u is v
+print (sum)
+#equl or is some diffaren 
+'''is cheek value and type
+but equl cheek only value'''
+#equl
+v1=3
+v2=3.0
+ch= v1==v2
+print(ch)
+#is operaotr
+v1=3
+v2=3.0
+ch= v1 is v2
+print (ch)
+
+#Bitwise operator
+bit=bin(3)
+print (bit)
+#bitwise and 
+bit1=5&4
+print(bit1)
+
