9336374974dab30a7a2450312a814b9713208775
shahedcsse <shahedhasan920@gmail>
2023-02-21 20:55:11 -0800

    folder handel

---
+++ Folderhandeling/folder.py
@@ -0,0 +1,17 @@
+import os
+#os.getcwd()
+
+#os.mkdir("folder2")
+#os.getcwd()
+
+#one time two folder create
+
+#os.makedirs('Folder3\Folder3.1')
+#os.removedirs("Folder3\Folder3.1")
+'''
+# walk operation
+
+for p,d,f in os.walk("."):
+    print(p,"||",d,"||",f)
+    
+    '''
