ccfb035a52b7843730f7859d20f2559af5591fa0
shahedcsse <shahedhasan920@gmail>
2023-02-22 21:13:05 -0800

    finalday7

---
+++ curentda.py
@@ -0,0 +1,31 @@
+import datetime
+import time
+
+today= datetime.date.today()
+print(today)
+
+f=today+datetime.timedelta(weeks=12, days=4)
+print(f)
+
+
+
+
+
+
+import datetime
+import time
+daycal=today+datetime.timedelta(days=9)
+print(daycal)
+
+
+hw=time.strftime("%d/%M/%y")
+print(hw)
+
+
+
+
+
+
+
+
+
