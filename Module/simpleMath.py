85a6a37e5bd404460469db5c94c07609cfe857f1
shahedcsse <shahedhasan920@gmail>
2023-03-04 18:18:17 -0800

    module

---
+++ Module/simpleMath.py
@@ -0,0 +1,8 @@
+def add(nam1:int, num2:int )->int:
+    return nam1+num2
+
+def sub(nam1:int, num2:int )->int:
+    return nam1-num2
+def mul(nam1:int, num2:int )->int:
+    return nam1*num2
+
---
+++ Module/testMo.py
@@ -0,0 +1,6 @@
+import simpleMath
+
+
+simpleMath.add (2,5)
+simpleMath.sub(7,3)
+simpleMath.mul(3,5)
